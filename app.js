83792c094da5a9a9ab5a0f8909b86a6daa361167
Hadia Shafique <hshafique@Hadias-MacBook-Air.local>
2022-11-03 13:45:42 -0500

    method possibly working

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-//#region Classes
+
+// let MyShip = new HumanShip('USS Generator')
+// let alienShipGenerator = new EvilShipFactory()
+//#region FactoryClass
 class EvilShipFactory {
     constructor() {
-        this.enemyShipCreator = []
+        this.enemyShipCollection = []
     }
     makeAlienShip(name) {
         const newAlienShip = new AlienShip(name)
-        this.enemyShipCreator.push(newAlienShip)
+        this.enemyShipCollection.push(newAlienShip)
     }
     printAlienShips() {
-        for (let AlienShip of this.enemyShipCreator) {
-          console.log(AlienShip);
+        for (let AlienShip of this.enemyShipCollection) {
+            console.log(AlienShip);
         }
-}
+    }
 }
 
+
+//#endregion
+//#region ClassHuman
 class HumanShip {
     constructor(name) {
         this.hull = 20
@@ -21,8 +27,26 @@ class HumanShip {
         this.accuracy = .7
         this.name = name
     }
-
+    attack(evilAlien) {
+        if (this.hull > 0) {
+            evilAlien.hull -= this.firepower
+            console.log(`I have ${evilAlien.hull} power left`)
+        } else {
+            if (evilAlien.hull > 3) {
+                evilAlien.firepower -= this.hull
+                console.log(`I have ${this.hull} power left`)
+            }
+        }
+    }
 }
+    
+   
+
+
+//#endregion
+
+
+//#region ClassAlien
 class AlienShip {
     constructor(name) {
         this.hull = this.randomHull(3, 6)
@@ -30,7 +54,7 @@ class AlienShip {
         this.accuracy = this.randomAccuracy()
         this.name = name
     }
-    
+
     randomHull(min, max) {
         return Math.floor(Math.random() * (max - min) + min)
 
@@ -42,26 +66,51 @@ class AlienShip {
         return (Math.floor(Math.random() * 3) + 6) / 10
     }
 }
-
-
-
-let AlienShipGenerator = new EvilShipFactory()
-AlienShipGenerator.makeAlienShip('Ship1')
-AlienShipGenerator.makeAlienShip('Ship2')
-AlienShipGenerator.makeAlienShip('Ship3')
-AlienShipGenerator.makeAlienShip('Ship4')
-AlienShipGenerator.makeAlienShip('Ship5')
-AlienShipGenerator.makeAlienShip('Ship6')
-console.log(AlienShipGenerator)
-
-
-let MyShip = new HumanShip('Hadia\'s Ship')
-
-console.log(MyShip)
-
-
-
-// let USSHelloWorld = new HumanShip
-// let CreateShipFactory = new ShipFactory
-
 //#endregion
+
+
+//#region Attackmethod instructions
+//You attack the first alien ship
+// If the ship survives, it attacks you
+// If you survive, you attack the ship again
+// If it survives, it attacks you again … etc
+// If you destroy the ship, you have the option to attack the next ship or to retreat
+// If you retreat, the game is over, perhaps leaving the game open for further developments or options
+// You win the game if you destroy all of the aliens
+// You lose the game if you are destroyed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#region ShipGen
+let alienShipGenerator = new EvilShipFactory()
+alienShipGenerator.makeAlienShip('Ship1')
+alienShipGenerator.makeAlienShip('Ship2')
+alienShipGenerator.makeAlienShip('Ship3')
+alienShipGenerator.makeAlienShip('Ship4')
+alienShipGenerator.makeAlienShip('Ship5')
+alienShipGenerator.makeAlienShip('Ship6')
+// console.log(alienShipGenerator)
+
+
+let MyShip = new HumanShip('USS Generator')
+
+// console.log(MyShip)
+//#endregion
+MyShip.attack(alienShipGenerator.enemyShipCollection[0])
+MyShip.attack(alienShipGenerator.enemyShipCollection[1])
+MyShip.attack(alienShipGenerator.enemyShipCollection[2])
+MyShip.attack(alienShipGenerator.enemyShipCollection[3])
+MyShip.attack(alienShipGenerator.enemyShipCollection[4])
+MyShip.attack(alienShipGenerator.enemyShipCollection[5])
